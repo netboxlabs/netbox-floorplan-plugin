413af7b6f63cd74ca4bce478ca036386943f03ab
Federico Favaro <federico.favaro@athonet.com>
2024-05-08 09:12:34 +0000

    Closes #22 Adapt plugin config import as per netbox 4.0 requirements

--- netbox_floorplan/__init__.py
+++ netbox_floorplan/__init__.py
@@ -1,11 +1,11 @@
-from extras.plugins import PluginConfig
+from netbox.plugins import PluginConfig
 
 
 class FloorplanConfig(PluginConfig):
     name = "netbox_floorplan"
     verbose_name = "Netbox Floorplan"
     description = ""
-    version = "0.3.6"
+    version = "0.3.7"
     base_url = "floorplan"
     min_version = "3.4.1"
 
